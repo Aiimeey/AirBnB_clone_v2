4b44b697d9414b2502be3730f57d85b2be6c90b3
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-19 09:28:31 -0800

    add setup mysql file fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,7 @@
--- Create database hbnb_dev_db
+--prepeares a mysql server for project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create a new user hbnb_dev identified by a password
--- SET GLOBAL validate_password.policy = 0;
+--create User if not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant select privileges on the performance_schema to the hbnb_dev user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
