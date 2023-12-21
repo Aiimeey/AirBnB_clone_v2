bf558dd457b1ae85fc391b6975c3551933a5ba67
imane <imanezenasnii@gmail.com>
2023-12-21 04:19:10 +0100

    update classes

--- models/amenity.py
+++ models/amenity.py
@@ -8,10 +8,10 @@ from os import getenv
 
 class Amenity(BaseModel, Base):
     """class Amenity"""
-if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
-            "Place", secondary="place_amenity", backref="amenities")
+            "Place", secondary="place_amenity", back_populates="amenities")
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -30,9 +30,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref="place", cascade="all,delete")
+        reviews = relationship('Review', backref="place", cascade="all,delete-orphan")
         amenities = relationship('Amenity', secondary="place_amenity",
-                                backref="place_amenities", viewonly=False)
+                                back_populates="place_amenities",
+                                viewonly=False)
     else:
         city_id = ""
         user_id = ""
