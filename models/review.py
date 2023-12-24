d334b31b8490c48f4dc247ed33903fa32bd74082
imane <imanezenasnii@gmail.com>
2023-12-24 03:59:01 +0100

    Apply corrections

--- models/review.py
+++ models/review.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-""" Review module """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.sql.schema import ForeignKey
-from os import getenv
+""" Review module for the HBNB project """
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """ Review class """
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+class Review(BaseModel):
+    """ Review classto store review information """
+    place_id = ""
+    user_id = ""
+    text = ""
--- models/state.py
+++ models/state.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                            cascade="all, delete-orphan")
-    else:
-        name = ""
-
-    @property
-    def cities(self):
-        """ Retrieve all the cities """
-        from models import storage
-        cities_list = []
-        dic = storage.all(City)
-        for city in dic.values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    name = ""
