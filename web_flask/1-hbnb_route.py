e1e4debeda12a3a0d8c6232eebc05eee4bbb139d
imane <imanezenasnii@gmail.com>
2024-02-21 18:51:07 +0100

    Route that displays 'HBNB'

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello_hbnb():
     """ Route that displays 'Hello HBNB!' """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Route that displays 'HBNB!' """
+    """ Route that displays 'HBNB' """
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
