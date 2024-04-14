1d08e6c7f5699117905b1b973124fbeed7228f7a
imane <imanezenasnii@gmail.com>
2024-04-14 21:22:48 +0100

    Add Flask script that displays a HTML page with the list of all states + cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-
-from flask import Flask, render_template
+"""this script starts a Flask web application
+    this web application must be listening on 0.0.0.0 at port 5000
+    Route:
+        /cities_by_states: ***
+"""
 from models import storage
+from models.state import State
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states/cites
+        at '/cities_by_states' path
+    """
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Method to handle teardown"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Display a HTML page with states and cities"""
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda x: x.name)
-
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
