c14d782849a1934ad71b7c84d5d1b33e94a1e4c1
imane <imanezenasnii@gmail.com>
2024-04-18 19:24:11 +0100

    update app.route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Route that displays 'Hello HBNB!' """
